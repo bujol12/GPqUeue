4dab3e8db618e4addb6a080c10c2febc0e64adec
wdhg <wdhgrant@gmail.com>
2021-06-02 13:58:56 +0100

    Setup basic navbar and page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
 import React from "react";
 import {Route, BrowserRouter as Router, Link} from "react-router-dom";
 
-const Navbar = () => {
+const NavbarLink = (props) => {
     return (
-        <React.Fragment>
-            <div>
-                <a><Link to="/">Home</Link></a>
-                <a><Link to="/login">Login</Link></a>
-            </div>
-        </React.Fragment>
+        <li className="nav-item">
+            <Link className="nav-link active" to={props.to}>{props.text}</Link>
+        </li>
     );
 };
 
-const Home = () => {
+const Navbar = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <React.Fragment>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">GPqUeue</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <NavbarLink to="/overview" text="Overview"/>
+                            <NavbarLink to="/myexperiments" text="My Experiments"/>
+                            <NavbarLink to="/gpus" text="GPUs" />
+                        </ul>
+                        <Link to="/newexperiment">
+                            <button className="btn btn-primary">
+                                New Experiment
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </React.Fragment>
     );
 };
 
@@ -28,13 +41,48 @@ const Login = () => {
     );
 };
 
+const Overview = () => {
+    return (
+        <div>
+            <h1>Overview</h1>
+        </div>
+    );
+};
+
+const MyExperiments = () => {
+    return (
+        <div>
+            <h1>My Experiments</h1>
+        </div>
+    );
+};
+
+const Gpus = () => {
+    return (
+        <div>
+            <h1>GPUs</h1>
+        </div>
+    );
+};
+
+const NewExperiment = () => {
+    return (
+        <div>
+            <h1>New Experiment</h1>
+        </div>
+    );
+};
+
 const App = () => {
     const routing = (
         <Router>
             <Navbar/>
             <div>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/login" component={Login}></Route>
+                <Route exact path="/" component={Login}></Route>
+                <Route exact path="/overview" component={Overview}></Route>
+                <Route exact path="/myexperiments" component={MyExperiments}></Route>
+                <Route exact path="/gpus" component={Gpus}></Route>
+                <Route exact path="/newexperiment" component={NewExperiment}></Route>
             </div>
         </Router>
     );
