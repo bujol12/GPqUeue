b7a5edbd5f105b94f8468f8fe607039b672d4833
wdhg <wdhgrant@gmail.com>
2021-06-02 12:11:42 +0100

    [thg19] Add basic React routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import {Route, BrowserRouter as Router, Link} from "react-router-dom";
 
-function App() {
+const Navbar = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
+        <React.Fragment>
+            <div>
+                <a><Link to="/">Home</Link></a>
+                <a><Link to="/login">Login</Link></a>
+            </div>
+        </React.Fragment>
+    );
+};
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
         </div>
     );
-}
+};
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+};
+
+const App = () => {
+    const routing = (
+        <Router>
+            <Navbar/>
+            <div>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/login" component={Login}></Route>
+            </div>
+        </Router>
+    );
+
+    return (
+        <div>
+            {routing}
+        </div>
+    );
+};
 
 export default App;
