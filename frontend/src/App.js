c7ca996c4515d925c8f766f107b6093948cbf9ef
wdhg <wdhgrant@gmail.com>
2021-06-03 19:55:35 +0100

    Handle submission of experiments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import {Route, BrowserRouter as Router, Link} from "react-router-dom";
+import React, {useState} from "react";
+import {Route, BrowserRouter as Router, Link, useHistory} from "react-router-dom";
+import axios from "axios";
 import Overview from "./Overview.js";
 import MyExperiments from "./MyExperiments.js";
 import {Login, SignUp} from "./LoginSignUp.js";
@@ -46,25 +47,53 @@ const Gpus = () => {
     );
 };
 
+const postNewJob = (history, name, file, args) => {
+    axios.post("/api/add_job", {
+        experiment_name: name,
+        script_path: file,
+        cli_args: args,
+    }).then(res => {
+        if (res.data.status === "success") {
+            history.push("/newexperiment/success");
+        } else {
+            history.push("/newexperiment/failed");
+        }
+    });
+};
+
 const NewExperiment = () => {
+    const [name, setName] = useState(undefined);
+    const [file, setFile] = useState(undefined);
+    const [args, setArgs] = useState("");
+
+    const history = useHistory();
+
+    const handleChange = (setter) => (e) => {
+        setter(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        postNewJob(history, name, file, args);
+    };
+
     return (
         <div className="container container-md-custom">
             <h1 className="pt-4 mb-4">Begin new experiment</h1>
-            <form action="/api/add_job">
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Experiment name*</label>
-                    <input type="text" className="form-control" id="name" aria-describedby="nameHelp" placeholder="e.g. Hot Dog vs Cold Dog Classifer"/>
+                    <input type="text" className="form-control" id="name" aria-describedby="nameHelp" placeholder="e.g. Hot Dog vs Cold Dog Classifer" onChange={handleChange(setName)}/>
                     <div id="nameHelp" className="form-text">Give your experiment a good unique name so it&apos;s easy to find later.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="script" className="form-label">Python / Bash script*</label>
-                    <input type="file" className="form-control" id="script" />
+                    <input type="file" className="form-control" id="script" onChange={handleChange(setFile)} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="arguments" className="form-label">Command line arguments</label>
-                    <input type="text" className="form-control" id="arguments" />
+                    <input type="text" className="form-control" id="arguments" onChange={handleChange(setArgs)} />
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
         </div>
     );
