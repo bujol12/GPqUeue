fe129d57141c2eda1c52118746a6fb5aebfefade
Joey Teng <joey.teng.dev@gmail.com>
2021-06-04 15:48:37 +0800

    feat(frontend): login / signup

--- frontend/src/LoginSignUp.js
+++ frontend/src/LoginSignUp.js
@@ -1,21 +1,50 @@
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+
 import "./LoginSignUp.css";
 
+const postForm = (history, type, name, pw) => {
+    axios.post("/api/" + type, {
+        username: name,
+        password: pw,
+    }).then(res => {
+        if (res.data.status === "success") {
+            history.push("/overview");
+        } else {
+            history.push("/" + type);
+        }
+    });
+};
+
+const handleChange = (setter) => (e) => {
+    setter(e.target.value);
+};
+
+
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+
+    const handleSubmit = () => {
+        postForm(history, "login", username, password);
+    };
+
+
     return (
         <div className="container container-sm-custom">
             <h1 className="pt-4 mb-4">Login</h1>
-            <form action="/api/login" >
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label">Username</label>
-                    <input type="text" className="form-control" id="username" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                </div>
-                <button type="submit" className="btn btn-primary login-button">Login</button>
-            </form>
+            <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input type="text" className="form-control" id="username" onChange={handleChange(setUsername)} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" onChange={handleChange(setPassword)} />
+            </div>
+            <button type="submit" className="btn btn-primary login-button" onClick={handleSubmit}>Login</button>
             <div className="text-center pt-3">
                 <Link to="signup">Or create an account</Link>
             </div>
@@ -24,20 +53,28 @@ const Login = () => {
 };
 
 const SignUp = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+
+    const handleSubmit = () => {
+        postForm(history, "signup", username, password);
+    };
+
+
     return (
         <div className="container container-sm-custom">
             <h1 className="pt-4 mb-4">Sign up</h1>
-            <form action="/api/signup" >
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label">Username</label>
-                    <input type="text" className="form-control" id="username" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                </div>
-                <button type="submit" className="btn btn-primary login-button">Sign up</button>
-            </form>
+            <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input type="text" className="form-control" id="username" onChange={handleChange(setUsername)} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" onChange={handleChange(setPassword)} />
+            </div>
+            <button type="submit" className="btn btn-primary login-button" onClick={handleSubmit}>Sign up</button>
             <div className="text-center pt-3">
                 <Link to="login">Have an account? Login instead</Link>
             </div>
@@ -45,4 +82,4 @@ const SignUp = () => {
     );
 };
 
-export {Login, SignUp};
+export { Login, SignUp };
