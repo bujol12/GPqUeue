d543154b49b1c76952b39f108172b30502d78c9d
tabsg <tabitha.goldman@btinternet.com>
2021-06-08 16:57:28 +0100

    [thg19][kdb19] change new experiment page to show cd and shell command examples

--- frontend/src/NewExperiment.js
+++ frontend/src/NewExperiment.js
@@ -2,11 +2,11 @@ import React, {useState} from "react";
 import {Link, useHistory} from "react-router-dom";
 import axios from "axios";
 
-const postNewJob = (history, name, file, args) => {
+const postNewJob = (history, name, command) => {
     axios.post("/api/add_job", {
         experiment_name: name,
-        script_path: file,
-        cli_args: args,
+        script_path: command,
+        cli_args: null,
     }).then(res => {
         if (res.data.status === "success") {
             history.push("/myexperiments");
@@ -16,10 +16,18 @@ const postNewJob = (history, name, file, args) => {
     });
 };
 
+const getCurrDir = () => {
+    axios.get("api/curr_dir").then(res => {
+        if (res.data.status === "success") {
+            return res.data.currDir;
+        }
+    });
+    return "no directory";
+};
+
 const NewExperiment = () => {
     const [name, setName] = useState(undefined);
-    const [file, setFile] = useState(undefined);
-    const [args, setArgs] = useState("");
+    const [command, setCommand] = useState(undefined);
 
     const history = useHistory();
 
@@ -28,8 +36,10 @@ const NewExperiment = () => {
     };
 
     const handleSubmit = () => {
-        postNewJob(history, name, file, args);
+        postNewJob(history, name, command);
     };
+
+    const currentDir = getCurrDir();
 
     return (
         <div className="container container-md-custom">
@@ -41,18 +51,22 @@ const NewExperiment = () => {
                     <div id="nameHelp" className="form-text">Give your experiment a good unique name so it&apos;s easy to find later.</div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="script_path" className="form-label">Python / Bash script*</label>
-                    <input type="file" className="form-control" id="script_path" onChange={handleChange(setFile)} />
+                    <label className="form-label">Current Directory:</label>
+                     <div id="nameHelp2" className="form-text"><code id="currentDir">{currentDir}</code></div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="cli_args" className="form-label">Command line arguments</label>
-                    <input type="text" className="form-control" id="cli_args" onChange={handleChange(setArgs)} />
+                    <label htmlFor="cli_command" className="form-label">Shell Command*</label>
+                    <input type="text" className="form-control" id="cli_command" onChange={handleChange(setCommand)} placeholder="e.g. python /.../model.py 1 2 3" />
+                    <div id="nameHelp" className="form-text">Examples:</div>
+                    <div id="nameHelp2" className="form-text"><code>sh /.../run.sh 1 2 3</code></div>
+                    <div id="nameHelp2" className="form-text"><code>pyenv activate model_env ; python /.../model.py</code></div>
                 </div>
                 <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
             </div>
         </div>
     );
 };
+
 
 const NewExperimentFailed = () => {
     return (
