39fe8d6e450a3ad3fce9ab075a6bc90835abe8eb
wdhg <wdhgrant@gmail.com>
2021-06-08 17:55:28 +0100

    Make GPU cards expandable

--- frontend/src/Gpus.js
+++ frontend/src/Gpus.js
@@ -22,19 +22,42 @@ const getGpus = (setGpus) => {
 const GPUCard = ({user, index, name, util, memory, maxMemory}) => {
     const icon = !user ? "available.png" : "busy.png";
     const userText = !user ? "Available" : user;
+    const collapseId = "gpuCardCollapse" + index;
+
+    let currentExperiments = [
+        {user: "Delilah Han", name: "Colddog vs Hotdog classifer"},
+        {user: "Joe Stacey", name: "Muffins vs Dogs Detector"},
+        {user: "Sherry Edwards", name: "Hotdog classifer"},
+    ].map((data, index) =>
+        <li key={index} className={"list-group-item" + (index == 0 ? " active" : "")}>
+            <span className="d-inline-flex w-100 justify-content-between">
+                {data.name}
+                <small>{data.user}</small>
+            </span>
+        </li>
+    );
 
     return (
         <div className="row border mb-3">
-            <div className="icon-col align-self-center pt-3 mb-3 me-3">
-                <img className="icon" src={icon} />
-            </div>
             <div className="col pt-3">
-                <h3>GPU {index} - {name}</h3>
-                <p>{userText}</p>
-            </div>
-            <div className="col pt-3 me-3 text-end">
-                <p>Utilisation: {(new Number(util * 100).toPrecision(3))}%</p>
-                <p>Memory: {memory} / {maxMemory} MiB</p>
+                <button className="row gpu-card" data-bs-toggle="collapse" data-bs-target={"#" + collapseId} aria-expanded="false" aria-controls="collapseOne">
+                    <div className="icon-col align-self-center mb-3 me-3">
+                        <img className="icon" src={icon} />
+                    </div>
+                    <div className="col text-start">
+                        <h3>GPU {index} - {name}</h3>
+                        <p>{userText}</p>
+                    </div>
+                    <div className="col pe-0 text-end">
+                        <p>Utilisation: {(new Number(util * 100).toPrecision(3))}%</p>
+                        <p>Memory: {memory} / {maxMemory} MiB</p>
+                    </div>
+                </button>
+                <div className="collapse row" id={collapseId}>
+                    <ul className="list-group pe-0 list-group-flush">
+                        {currentExperiments}
+                    </ul>
+                </div>
             </div>
         </div>
     );
