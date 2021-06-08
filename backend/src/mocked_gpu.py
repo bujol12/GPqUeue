add6404f71850cfe378909d972bc8e6c55f6fe25
wdhg <wdhgrant@gmail.com>
2021-06-08 17:00:54 +0100

    Randomly generate users in MockedGPU, filter user data in get_stats

--- backend/src/mocked_gpu.py
+++ backend/src/mocked_gpu.py
@@ -5,10 +5,17 @@ import attr
 
 from src.enums.job_status import JobStatus
 from src.gpu import GPU
+from src.user import User
 
 
 @attr.define(slots=False)
 class MockedGPU(GPU):
+    users = [
+        "Delilah Han",
+        "Joe Stacey",
+        "Sherry Edwards"
+    ]
+
     def get_stats(self) -> Dict[str, Union[Optional[str], float]]:
         self.fetch_stats()
 
@@ -16,7 +23,10 @@ class MockedGPU(GPU):
 
     def fetch_stats(self) -> None:
         self.last_status = random.choice([e.value for e in JobStatus])
-        self.last_user = None
+        if random.random() < 0.5:
+            self.last_user = None
+        else:
+            self.last_user = User(username=random.choice(self.users), hashed_pw="")
 
         self.last_utilisation_pct = random.uniform(0, 1)
         self.last_memory_used_mib = float(
