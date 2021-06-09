3c678868395538bc104493da7675936490fd5e10
Joey Teng <joey.teng.dev@gmail.com>
2021-06-10 03:13:31 +0800

    feat(frontend): getExperiments: allow displaying more info

--- frontend/src/MyExperiments.js
+++ frontend/src/MyExperiments.js
@@ -10,9 +10,11 @@ const getExperiments = (setExperiments, endpoint) => {
                 name: res.data.jobs[key].name,
                 path: res.data.jobs[key].script_path,
                 uuid: res.data.jobs[key].uuid,
-                user: res.data.jobs[key].user,
+                user: `${res.data.jobs[key].user.username}`,
                 status: res.data.jobs[key].status,
                 gpus: res.data.jobs[key].gpus_list,
+                start: res.data.jobs[key].start_time,
+                end: res.data.jobs[key].end_time,
             });
         }
         setExperiments(tempExperiments);
