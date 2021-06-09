93943072d986557b596d1dd50a8f3ed013bed4a3
Joey Teng <joey.teng.dev@gmail.com>
2021-06-09 23:43:05 +0800

    fix(frontend): utils: better resultant string when time is undefined

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -2,6 +2,9 @@ const secondsToHoursMinutesSeconds = (totalSeconds) => {
     const seconds = totalSeconds % 60;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const hours = Math.floor(totalSeconds / (60 * 60));
+    if (isNaN(seconds) || isNaN(minutes) || isNaN(hours)) {
+        return "Not Available";
+    }
     return `${hours}h ${minutes}m ${seconds}s`;
 };
 
