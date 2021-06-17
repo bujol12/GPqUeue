7762d74b856eab595e58d7c4add72727ba6b91aa
Joey Teng <joey.teng.dev@gmail.com>
2021-06-17 23:48:00 +0800

    test(backend): adapt to the type change of Job.cli_args

--- backend/test/test_job.py
+++ backend/test/test_job.py
@@ -1,9 +1,13 @@
 import pytest
-from src.job import Job
 from src.enums.job_status import JobStatus
+from src.job import Job
+
 
 class TestJob:
-    job = Job(project="General", name="abc", script_path="xyz/hjk", cli_args="-x")
+    job = Job(
+        project="General", name="abc",
+        script_path="xyz/hjk", cli_args={"-x": ""}
+    )
 
     def test_job_to_dict(self):
         dct_res = self.job.to_dict()
@@ -11,7 +15,7 @@ class TestJob:
         assert dct_res.get('status', '') == JobStatus.QUEUED.value
         assert dct_res.get('name', '') == 'abc'
         assert dct_res.get('script_path', '') == 'xyz/hjk'
-        assert dct_res.get('cli_args', '') == '-x'
+        assert dct_res.get('cli_args', '') == {'-x': ''}
 
         assert len(dct_res.keys()) == 7
         assert 'uuid' in dct_res.keys()
